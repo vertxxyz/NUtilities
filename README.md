712365dce515c65109e816a57b7cce9e821153f4
Thomas Ingram <thomasingram@vertx.xyz>
2020-02-17 22:00:47 +1100

    Added Instance Pool and updated Readme and Changelog.

--- README.md
+++ README.md
@@ -4,15 +4,45 @@ A collection of various tools and utilities for Unity.
 ## 🚨 WORK IN PROGRESS 🚨
 This is a platform for adding utilities to Unity as I see fit.
 
+## Contents:
+##### Utilities:
 - [Material Property Remapper](https://github.com/vertxxyz/NUtilities/wiki/MaterialPropertyRemapper)
-- [Package Updater](https://github.com/vertxxyz/NUtilities/wiki/Package-Updater)
-- [EnumToValue](https://github.com/vertxxyz/NUtilities/wiki/EnumToValue)
+- [Package Updater](https://github.com/vertxxyz/NUtilities/wiki/Package-Updater) (Requires [Newtonsoft Json](http://docs.unity3d.com/Packages/com.unity.nuget.newtonsoft-json@latest))
 - [Code Generation](https://github.com/vertxxyz/NUtilities/wiki/Code-Generation)
+
+##### Editor Helper Classes:
+- EditorUtils
+- EditorGUIExtensions
+- StyleExtensions
+
+##### Runtime Helper Classes:
+- Instance Pool
+- Proportional Values
+
+##### Property Attributes:
+- [EnumToValue](https://github.com/vertxxyz/NUtilities/wiki/EnumToValue)
+- EnumFlags
+- CurveDisplay
+- ReadOnly
+- EditorOnly
+- Keycode
+- MinMax
+- Progress
+- Blend2D
+- File
+
+##### UIElement Controls:
+- Drag and Drop Box
+- Help Box
+
+##### Improved Inspectors:
+ - Animator
+ - Scriptable Object
 
 ----
 [Utilities relating to timeline](https://github.com/vertxxyz/NTimeline) have their own package separate to this.
 
-Other important tools I have made may remain separate to this, like:
+Other important tools I have made may remain separate to this:
 - [NTexturePreview](https://github.com/vertxxyz/NTexturePreview) - Enhanced Texture Previewer for Unity
 - [NSelection](https://github.com/vertxxyz/NSelection) - Simple selection for busy scenes in Unity
 
@@ -23,7 +53,8 @@ Open utility windows under: **Window/Vertx/...**
 See the [wiki](https://github.com/vertxxyz/NUtilities/wiki) for explanations of each utility.
 
 ## Installation
-Edit your `manifest.json` file to contain `"com.vertx.nutilities": "https://github.com/vertxxyz/NUtilities.git",`
+Edit your `manifest.json` file to contain `"com.vertx.nutilities": "https://github.com/vertxxyz/NUtilities.git",`,
+or if using 2019.3< the **+** button in **Window/Package Manager**, and *Add package from Git URL.*
 
 To update the package with new changes, remove the lock from the bottom of the `manifest.json` file.
 Or add the package to a [Package Updater](https://github.com/vertxxyz/NUtilities/wiki/Package-Updater) Scriptable Object and update via the interface.
---
+++ Runtime/Core/Logic/InstancePool.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Vertx
+{
+	/// <summary>
+	/// A pool for Component instances.
+	/// </summary>
+	/// <typeparam name="TInstanceType">The Component Type associated with the pool</typeparam>
+	public static class InstancePool<TInstanceType> where TInstanceType : Component
+	{
+		/// <summary>
+		/// Dictionary of prefab components to stacks of pooled instances.
+		/// </summary>
+		private static readonly Dictionary<TInstanceType, Stack<TInstanceType>> pool = new Dictionary<TInstanceType, Stack<TInstanceType>>();
+
+		/// <summary>
+		/// Retrieves an instance from the pool, positioned at the origin.
+		/// </summary>
+		/// <param name="prefab">The prefab key to retrieve instances of.</param>
+		/// <param name="parent">The parent to parent instances under.</param>
+		/// <returns>An instance retrieved from the pool.</returns>
+		public static TInstanceType Get(TInstanceType prefab, Transform parent = null)
+			=> Get(prefab, parent, Vector3.zero, Quaternion.identity);
+
+		/// <summary>
+		/// Retrieves a positioned instance from the pool.
+		/// </summary>
+		/// <param name="prefab">The prefab key to retrieve instances of.</param>
+		/// <param name="parent">The parent to parent instances under.</param>
+		/// <param name="position">Position of the instance</param>
+		/// <param name="rotation">Rotation of the instance</param>
+		/// <param name="space">Which space the position and rotation is applied in</param>
+		/// <returns>An instance retrieved from the pool.</returns>
+		public static TInstanceType Get(TInstanceType prefab, Transform parent, Vector3 position, Quaternion rotation, Space space = Space.World)
+		{
+			// Use the pool if we have one already
+			if (pool.TryGetValue(prefab, out var stack))
+			{
+				if (stack.Count > 0)
+				{
+					TInstanceType poppedInstance;
+					bool found;
+					do
+					{
+						//Iterate to remove null items from the stack
+						poppedInstance = stack.Pop();
+						found = poppedInstance != null;
+					} while (!found && stack.Count > 0);
+
+					if (found)
+					{
+						// Activate and re-parent
+						poppedInstance.gameObject.SetActive(true);
+						Transform t = poppedInstance.transform;
+						if (t.parent != parent)
+							t.SetParent(parent);
+						
+						//Position
+						switch (space)
+						{
+							case Space.World:
+								t.SetPositionAndRotation(position, rotation);
+								break;
+							case Space.Self:
+								t.localPosition = position;
+								t.localRotation = rotation;
+								break;
+							default:
+								throw new ArgumentOutOfRangeException(nameof(space), space, null);
+						}
+
+						return poppedInstance;
+					}
+				}
+			}
+
+			// Otherwise return a new instance.
+			// Only when an instance is returned do we need to create a pool.
+			TInstanceType instance;
+			// Position
+			switch (space)
+			{
+				case Space.World:
+					instance = Object.Instantiate(prefab, position, rotation, parent);
+					break;
+				case Space.Self:
+					instance = Object.Instantiate(prefab, parent);
+					Transform t = instance.transform;
+					t.localPosition = position;
+					t.localRotation = rotation;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(space), space, null);
+			}
+
+			return instance;
+		}
+
+		/// <summary>
+		/// Returns a Component instance to the pool.
+		/// </summary>
+		/// <param name="prefab">The prefab key used when the instance was retrieved via <see cref="Get(TInstanceType,UnityEngine.Transform)"/></param>
+		/// <param name="instance">The instance to return to the pool.</param>
+		public static void Pool(TInstanceType prefab, TInstanceType instance)
+		{
+			// Create a pool if we don't have one already.
+			if (!pool.TryGetValue(prefab, out var stack))
+			{
+				stack = new Stack<TInstanceType>();
+				pool.Add(prefab, stack);
+			}
+
+			// Disable the object and push it to the stack.
+			instance.gameObject.SetActive(false);
+			stack.Push(instance);
+		}
+
+		/// <summary>
+		/// If you are temporarily working with pools for prefabs you can remove them from the system by calling this function.
+		/// </summary>
+		/// <param name="prefab">The prefab key referring to the pool.</param>
+		public static void RemovePrefabPool(TInstanceType prefab)
+		{
+			if (pool.ContainsKey(prefab))
+				pool.Remove(prefab);
+		}
+	}
+}
