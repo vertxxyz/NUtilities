4ddddfc88a69af932d5afdfd5fb70692d06194d9
Thomas Ingram <thomasingram@vertx.xyz>
2020-03-02 08:40:40 +1100

    Updated changelog and package.json. Added default display to Asset List.
    +Minor formatting.

--- Editor/AssetList/AssetListUtility.cs
+++ Editor/AssetList/AssetListUtility.cs
@@ -383,42 +383,27 @@ namespace Vertx.Editor
 					propertyName = "EnumDisplay";
 					break;
 				case SerializedPropertyType.Vector2:
-					break;
 				case SerializedPropertyType.Vector3:
-					break;
 				case SerializedPropertyType.Vector4:
-					break;
 				case SerializedPropertyType.Rect:
-					break;
 				case SerializedPropertyType.ArraySize:
-					break;
 				case SerializedPropertyType.Character:
-					break;
 				case SerializedPropertyType.AnimationCurve:
-					break;
 				case SerializedPropertyType.Bounds:
-					break;
 				case SerializedPropertyType.Gradient:
-					break;
 				case SerializedPropertyType.Quaternion:
-					break;
 				case SerializedPropertyType.ExposedReference:
-					break;
 				case SerializedPropertyType.FixedBufferSize:
-					break;
 				case SerializedPropertyType.Vector2Int:
-					break;
 				case SerializedPropertyType.Vector3Int:
-					break;
 				case SerializedPropertyType.RectInt:
-					break;
 				case SerializedPropertyType.BoundsInt:
-					break;
 				case SerializedPropertyType.ManagedReference:
-					break;
 				default:
-					throw new ArgumentOutOfRangeException();
+					propertyName = "DefaultDisplay";
+					break;
 			}
+
 			return propertyName;
 		}
 
